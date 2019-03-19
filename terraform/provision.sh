d329221375d57f00a5f1ea2c4a05bc98c787534d
Dhananjay Balan <mail@dbalan.in>
2019-03-19 15:06:16 -0400

    Run terraform from provision file

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+# apply terraform (set DO_TOKEN)
+terraform apply -var=do_token=${DO_TOKEN}
+
+# get floating IP
 IP=$(terraform output | cut -d '=' -f2 | tr -d ' ')
 ssh root@${IP} apt-get -y install python
 ansible-playbook -i ${IP}, gateway/gateway.yml
+# manually set the dns if you need to.
 
 
