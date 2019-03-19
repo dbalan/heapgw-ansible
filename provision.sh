f366b45c27d39fecc1258104bb8baea6b97e6ec1
Dhananjay Balan <mail@dbalan.in>
2019-03-19 15:23:46 -0400

    Machine takes a while to ramp up.

--- provision.sh
+++ provision.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+set -e
+
 # apply terraform (set DO_TOKEN)
 terraform apply -var=do_token=${DO_TOKEN}
 
 # get floating IP
 IP=$(terraform output | cut -d '=' -f2 | tr -d ' ')
+
+# WAIT for to boot
+sleep 30
 ssh root@${IP} apt-get -y install python
 ansible-playbook -i ${IP}, gateway/gateway.yml
 # manually set the dns if you need to.
